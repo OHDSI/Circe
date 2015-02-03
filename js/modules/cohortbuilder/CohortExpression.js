ad89bca37254f7ab794f54aee41cfa1e5748ecf8
Chris Knoll <cknoll@ohdsi.org>
2015-02-02 21:36:37 -0500

    Fixed creating blank expression.  PrimaryCriteria should always be instantiated, even if it results in an empty object.

--- js/modules/cohortbuilder/CohortExpression.js
+++ js/modules/cohortbuilder/CohortExpression.js
@@ -9,7 +9,7 @@ define(function (require, exports) {
 		var self = this;
 		var data = data || {};
 
-		self.PrimaryCriteria = ko.observable(data.PrimaryCriteria && new PrimaryCriteria(data.PrimaryCriteria));
+		self.PrimaryCriteria = ko.observable(new PrimaryCriteria(data.PrimaryCriteria));
 		self.AdditionalCriteria = ko.observable(data.AdditionalCriteria && new CriteriaGroup(data.AdditionalCriteria));
 		self.Codesets = ko.observableArray(data.Codesets && data.Codesets.map(function(d) { return new Codeset(d) }));
 		self.ExpressionLimit =  { Type: ko.observable(data.ExpressionLimit || "All") }
